d23d9e74850285c7d15003becc450c87ac27bf87
Connor <wscofield@elon.edu>
2019-02-19 10:15:45 -0500

    initializing all first angular app (Angular 7 Udemy Course) to a git repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import {HttpModule} from '@angular/http'; 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { TextComponent } from './text/text.component';
+import { ServersComponent } from './servers/servers.component';
+import { DanielaComponent } from './daniela/daniela.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    TextComponent,
+    ServersComponent,
+    DanielaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/text/text.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-text-guy',
+    templateUrl: './text.component.html'
+})
+
+export class TextComponent {};
